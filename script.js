60d5115d7fec2e321cb666396cb4a31c8a16664e
Alina <alinasolnce99@gmail.com>
2021-04-12 15:20:16 +0300

    finished with video container

---
+++ script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
